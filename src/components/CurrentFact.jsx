48fcd332bd7f062a29a53066b72a49c8b85bd34b
mleray <maud.leray035@gmail.com>
2019-04-12 10:42:56 +0200

    Small updates in CurrentFact component

--- src/components/CurrentFact.jsx
+++ src/components/CurrentFact.jsx
@@ -3,17 +3,30 @@ import PropTypes from 'prop-types';
 
 // Material-UI components
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-function CurrentFact({ fact }) {
-    return (
-        <Typography component='p'>
-            {fact}
-        </Typography>
-    );
+const styles = {
+	container: {
+		marginTop: 20,
+		maxWidth: '50%',
+		margin: 'auto'
+	}
+};
+
+function CurrentFact({ classes, fact }) {
+	return (
+		<div className={classes.container}>
+			<Typography variant="h5" gutterBottom>
+				Latest fact:
+			</Typography>
+			<Typography component="p">{fact}</Typography>
+		</div>
+	);
 }
 
 CurrentFact.propTypes = {
-    fact: PropTypes.string
+	classes: PropTypes.object.isRequired,
+	fact: PropTypes.string
 };
 
-export default CurrentFact;
+export default withStyles(styles)(CurrentFact);
