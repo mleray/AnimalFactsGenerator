94966b84090c6fa55b517f928c61bf3c20e5bf0b
mleray <maud.leray035@gmail.com>
2019-04-12 15:04:16 +0200

    Removed try/catch

--- src/api/requests.js
+++ src/api/requests.js
@@ -3,12 +3,8 @@ const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const apiUrl = 'https://some-random-api.ml/facts';
 
 function getAnimalFact(animal = 'cat') {
-    try {
-        return fetch(`${proxyUrl + apiUrl}/${animal}`)
-            .then(response => response.json());
-    } catch (error) {
-        console.log(error);
-    }
+    return fetch(`${proxyUrl + apiUrl}/${animal}`)
+        .then(response => response.json());
 }
 
 export {
