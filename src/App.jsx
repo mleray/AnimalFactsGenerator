2fcb1efe0d3f1c5f9d9823ba2b394a6073fa08bc
mleray <maud.leray035@gmail.com>
2019-04-12 10:42:09 +0200

    Added useEffect in App to update document title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Material-UI components
 import Grid from '@material-ui/core/Grid';
@@ -15,37 +15,43 @@ import ANIMALS from './constants/animals';
 import { getAnimalFact } from './api/requests';
 
 function App() {
-    const [currentFact, setCurrentFact] = useState('');
-    const [disabled, setDisabled] = useState(false);
+	const [currentFact, setCurrentFact] = useState('');
+	const [disabled, setDisabled] = useState(false);
+	const [savedFacts, setSavedFacts] = useState([]);
 
-    const getFact = (animal) => {
-        setDisabled(true);
-        getAnimalFact(animal).then((response) => {
-            setCurrentFact(response.fact);
-            setDisabled(false);
-        });
-    }
+	const getFact = animal => {
+		setDisabled(true);
+		getAnimalFact(animal).then(response => {
+			setCurrentFact(response.fact);
+			setDisabled(false);
+		});
+	};
 
-    return (
-        <div style={{ textAlign: 'center', padding: 50 }}>
-            <Typography variant='h4' gutterBottom>
-                Random animal fact generator
-            </Typography>
-            <Grid container justify='center' spacing={24}>
-                {Object.keys(ANIMALS).map(animal => (
-                    <Grid key={animal} item>
-                        <AnimalCard
-                            animal={ANIMALS[animal].name}
-                            disabled={disabled}
-                            getFact={getFact}
-                            image={ANIMALS[animal].image}
-                        />
-                    </Grid>
-                ))}
-            </Grid>
-            {currentFact && <CurrentFact fact={currentFact} />}
-        </div>
-    );
+	useEffect(() => {
+		document.title = `Saved facts: ${savedFacts.length}`;
+	}, [savedFacts]);
+
+	return (
+		<div style={{ textAlign: 'center', padding: 50 }}>
+			<Typography variant="h4" gutterBottom>
+				Random animal fact generator{' '}
+			</Typography>{' '}
+			<Grid container justify="center" spacing={24}>
+				{' '}
+				{Object.keys(ANIMALS).map(animal => (
+					<Grid key={animal} item>
+						<AnimalCard
+							animal={ANIMALS[animal].name}
+							disabled={disabled}
+							getFact={getFact}
+							image={ANIMALS[animal].image}
+						/>{' '}
+					</Grid>
+				))}{' '}
+			</Grid>{' '}
+			{currentFact && <CurrentFact fact={currentFact} />}{' '}
+		</div>
+	);
 }
 
 export default App;
